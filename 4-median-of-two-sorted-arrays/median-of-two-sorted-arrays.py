4bbeb60fa508a5bdea5fe4236a0d0124a42bf896
Gibran Myageri <myageri.g@northeastern.edu>
2025-04-01 23:42:07 -0400

    Time: 5 ms (24.32%) | Memory: 17.9 MB (84.07%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,11 +1,36 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged_arr = sorted(nums1 + nums2)
-        n = len(merged_arr)
+        # O(nlogn) solution
+        # merged_arr = sorted(nums1 + nums2)
+        # n = len(merged_arr)
 
+        # if n % 2 == 0:
+        #     return (merged_arr[int((n/2) - 1)] + merged_arr[int((n/2))]) / 2
+        # else:
+        #     return merged_arr[n // 2]
+        
+        # merge the 2 arrays
+        m_arr = []
+        i = 0
+        j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] <= nums2[j]:
+                m_arr.append(nums1[i])
+                i += 1
+            else:
+                m_arr.append(nums2[j])
+                j += 1
+        
+        if i < len(nums1):
+            m_arr.extend(nums1[i:])
+        if j < len(nums2):
+            m_arr.extend(nums2[j:])
+
+        print(m_arr)
+        
+        n = len(m_arr)
+        
         if n % 2 == 0:
-            return (merged_arr[int((n/2) - 1)] + merged_arr[int((n/2))]) / 2
+            return (m_arr[int((n/2) - 1)] + m_arr[int((n/2))]) / 2
         else:
-            return merged_arr[n // 2]
-        
-        
+            return m_arr[n // 2]
