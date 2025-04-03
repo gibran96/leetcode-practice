111f877462b0b38d5a37c81b91355b152d1c4bef
Gibran Myageri <myageri.g@northeastern.edu>
2025-04-02 20:29:21 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.58%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -6,12 +6,29 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # recursive sol
+        # res = []
+        # def postorder(root):
+        #     if not root:
+        #         return
+        #     postorder(root.left)
+        #     postorder(root.right)
+        #     res.append(root.val)
+        # postorder(root)
+        # return res
+
+        # iterative sol
         res = []
-        def postorder(root):
-            if not root:
-                return
-            postorder(root.left)
-            postorder(root.right)
-            res.append(root.val)
-        postorder(root)
+        stack = []
+        cur = root
+
+        while cur or stack:
+            if cur:
+                res.append(cur.val)
+                stack.append(cur)
+                cur = cur.right
+            else:
+                cur = stack.pop()
+                cur = cur.left
+        res.reverse()
         return res
